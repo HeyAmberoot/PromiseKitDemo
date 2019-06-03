7b200038219b86f8469fd02dc5d8e5346ab4d5ba
HeyAmberoot <905902352@qq.com>
2019-06-03 15:55:14 +0800

    update photo

--- PromiseKitDemo/ViewController.swift
+++ PromiseKitDemo/ViewController.swift
@@ -12,7 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+       
+        
+        
+    }
+    
+    func savePhoto() {
+        let image = UIImage(named: "demo")!
+        PhotoAlbumUtil.saveImageInAlbum(image: image, albumName: "amberoot") { (result) in
+            switch result{
+            case .success:
+                print("保存成功")
+            case .denied:
+                print("被拒绝")
+            case .error:
+                print("保存错误")
+            }
+        }
     }
 
 
