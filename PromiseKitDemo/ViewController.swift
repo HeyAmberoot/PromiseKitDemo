c5ef51e77225b03e63acb86d6d5085c94ebefdbc
HeyAmberoot <905902352@qq.com>
2019-06-03 17:15:20 +0800

    cook

--- PromiseKitDemo/ViewController.swift
+++ PromiseKitDemo/ViewController.swift
@@ -7,17 +7,71 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+//        firstly {
+//            fetch()
+//
+//        }.then {
+//            map($0)
+//        }.then {
+//            set($0)
+//            return animate()
+//        }.ensure {
+//            // something that should happen whatever the outcome
+//        }.catch {
+//            handle(error: $0)
+//        }
+        _ = cook()
+            .then(eat)
+            .then(wash)
+            .done { data in
+                print(data)
+        }
         
-       
-        
-        
+    }
+    //做饭
+    func cook() -> Promise<String> {
+        print("开始做饭。")
+        let p = Promise<String> { resolver in
+            //做一些异步操作(延迟1秒执行)
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                print("做饭完毕！")
+                resolver.fulfill("鸡蛋炒饭")
+            }
+        }
+        return p
+    }
+    
+    //吃饭
+    func eat(data:String) -> Promise<String> {
+        print("开始吃饭：" + data)
+        let p = Promise<String> { resolver in
+            //做一些异步操作(延迟1秒执行)
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                print("吃饭完毕！")
+                resolver.fulfill("一块碗和一双筷子")
+            }
+        }
+        return p
+    }
+    
+    //洗碗
+    func wash(data:String) -> Promise<String> {
+        print("开始洗碗：" + data)
+        let p = Promise<String> { resolver in
+            //做一些异步操作(延迟1秒执行)
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                print("洗碗完毕！")
+                resolver.fulfill("干净的碗筷")
+            }
+        }
+        return p
     }
     
     func savePhoto() {
